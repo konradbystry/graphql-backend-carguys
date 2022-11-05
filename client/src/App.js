36f5f812a302463c7d8df97d78811f274d13fbed
Konrad Bystrzykowski <k.bystrzykowski@testspring.pl>
2022-11-05 19:53:42 +0100

    added protected paths

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,33 @@
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Login from './pages/auth/Login';
-import Register from './pages/auth/Register';
-import Navbar from './components/navbar';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import Navbar from "./components/navbar";
+import Topic from "./pages/Topic";
+import TopicsList from "./pages/TopicsList";
+import User from "./pages/User";
+import NewTopic from "./pages/NewTopic";
+import ProtectedRoutes from "./components/ProtectedRoutes";
 
 function App() {
   return (
-      <div>
-        <Navbar/>
-        <Routes>
-          <Route path='/' element={<Home/>} />
-          <Route path='/login' element={<Login/>} />
-          <Route path='/register' element={<Register/>} />
-        </Routes>
-      </div>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/topics">
+          <Route index element={<TopicsList />} />
+          <Route path=":id" element={<Topic />} />
+          <Route element={<ProtectedRoutes />}>
+            <Route path="new" element={<NewTopic />} />
+          </Route>
+        </Route>
+        <Route path="/user/:id" element={<User />} />
+      </Routes>
+    </div>
   );
 }
 
