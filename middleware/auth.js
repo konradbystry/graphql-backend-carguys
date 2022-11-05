eae9aea5c7d4901bbaf1de31c34b3b745cec2081
Konrad Bystrzykowski <k.bystrzykowski@testspring.pl>
2022-11-05 19:56:59 +0100

    updated middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,20 +1,20 @@
-const { AuthenticationError } = require('apollo-server')
+const { AuthenticationError } = require("apollo-server");
 
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 
 module.exports = (context) => {
-    const authHeader = context.req.headers.authorization
-    if(authHeader) {
-        const token = authHeader.split('Bearer')[1]
-        if (token) {
-            try {
-                const user = jwt.verify('token', 'TEMP_STRING')
-                return user
-            } catch {
-                throw new AuthenticationError('Invalid/Expired token')
-            }
-        }
-        throw new Error('Authentication token must be "Bearer [token]"')
+  const authHeader = context.req.headers.authorization;
+  if (authHeader) {
+    const token = authHeader.split("Bearer")[1];
+    if (token) {
+      try {
+        const user = jwt.verify("token", "TEMP_STRING");
+        return user;
+      } catch {
+        throw new AuthenticationError("Invalid/Expired token");
+      }
     }
-    throw new Error('Authorization header must be provided')
-}
+    throw new Error('Authentication token must be "Bearer [token]"');
+  }
+  throw new Error("Authorization header must be provided");
+};
