505df10acf15e59be967a98e8aa7cac3c8a8eb8e
Konrad Bystrzykowski <k.bystrzykowski@testspring.pl>
2022-11-15 22:49:53 +0100

    updated post types and resolvers

--- graphql/schema/Post/post.resolvers.js
+++ graphql/schema/Post/post.resolvers.js
@@ -1,34 +1,37 @@
-
-const User = require('../../../models/User')
-const Post = require('../../../models/Post')
-const Topic = require('../../../models/Topic')
+const User = require("../../../models/User");
+const Post = require("../../../models/Post");
+const Topic = require("../../../models/Topic");
+const date = require("date-and-time");
 // const {ObjectId} = require('mongodb')
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 module.exports = {
-    Query: {
-        async getPosts(_, {topicId}) {
-            return await Post.find({topicId})
-        },
-        async getPost(_, {ID}){
-            return await Post.findById(ID)
-        }
+  Query: {
+    async getPosts(_, { topicId }) {
+      return await Post.find({ topicId });
     },
-    Mutation: {
-        async createPost(_, {postInput: {text, userId, topicId}}){
-            const createdPost = new Post({
-                text : text,
-                userId : userId,
-                topicId : topicId,
-                date : "",
-                image : ""
-            })
+    async getPost(_, { ID }) {
+      return await Post.findById(ID);
+    },
+  },
+  Mutation: {
+    async createPost(_, { postInput: { text, userId, userName, topicId } }) {
+      const now = new Date();
 
-            const res = await createdPost.save()
+      const createdPost = new Post({
+        text: text,
+        userId: userId,
+        userName: userName,
+        topicId: topicId,
+        date: date.format(now, "ddd, MMM DD YYYY"),
+        image: "",
+      });
 
-            return {
-                id: res.id,
-                ...res._doc
-            }
-        }        
-    }
-}
+      const res = await createdPost.save();
+
+      return {
+        id: res.id,
+        ...res._doc,
+      };
+    },
+  },
+};
--- graphql/schema/Post/post.typeDefs.js
+++ graphql/schema/Post/post.typeDefs.js
@@ -1,29 +1,31 @@
-const {gql} = require('graphql-tag')
-const mongoose = require('mongoose')
+const { gql } = require("graphql-tag");
+const mongoose = require("mongoose");
 
 module.exports = gql`
-    type Post{
-        userId: String,
-        topicId: String,
-        date: String,
-        text: String,
-        image: String
-    }
+  type Post {
+    userId: String
+    userName: String
+    topicId: String
+    date: String
+    text: String
+    image: String
+  }
 
-    input PostInput{
-        text: String,
-        userId : String,
-        topicId : String
-    }
+  input PostInput {
+    text: String
+    userId: String
+    userName: String
+    topicId: String
+  }
 
-    type Query{
-        getPosts(topicId: String) : [Post]
-        getPost(ID: ID!): Post!
-    }
+  type Query {
+    getPosts(topicId: String): [Post]
+    getPost(ID: ID!): Post!
+  }
 
-    type Mutation{
-        createPost(postInput : PostInput): Post! #to do - add params userId and topicId
-        editPost(ID: ID!, postInput: PostInput): Boolean
-        deletePost(ID: ID!): Boolean
-    }
-`
+  type Mutation {
+    createPost(postInput: PostInput): Post! #to do - add params userId and topicId
+    editPost(ID: ID!, postInput: PostInput): Boolean
+    deletePost(ID: ID!): Boolean
+  }
+`;
